57c2fc1330c0c87a6d7b1dd8fc268ead407d7e01
Niteshkr35 <niteshkr35@gmail.com>
2021-04-02 15:13:46 +0530

    created pages and configured navigations links

--- Pages/views.py
+++ Pages/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'Pages/home.html')
+
+def about(request):
+    return render(request,'Pages/about.html')
+
+def cars(request):
+    return render(request,'Pages/cars.html')
+
+def services(request):
+    return render(request,'Pages/services.html')
+
+def contact(request):
+    return render(request,'Pages/contact.html')
