7c48f23fe268cb07aa5165680eb55f73e51c06de
Niteshkr35 <niteshkr35@gmail.com>
2021-04-05 00:20:31 +0530

    Added Cars app and fetched the data into homepage

--- Pages/views.py
+++ Pages/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Team
+from cars.models import Car
 
 # Create your views here.
 def home(request):
     teams = Team.objects.all()
+    featured_cars = Car.objects.order_by('-created_date').filter(is_featured = True)
+    all_cars = Car.objects.order_by('-created_date')
     data = {
         'teams':teams,
+        'featured_cars':featured_cars,
+        'all_cars' : all_cars,
     }
     return render(request,'Pages/home.html',data)
 
@@ -15,9 +20,6 @@ def about(request):
         'teams': teams,
     }
     return render(request,'Pages/about.html',data)
-
-def cars(request):
-    return render(request,'Pages/cars.html')
 
 def services(request):
     return render(request,'Pages/services.html')
